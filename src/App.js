9e3ab049ed65d0ca04ba96f23e348f1944f551b7
Antoine <tadgithub@gmail.com>
2022-04-12 11:34:14 -0400

    Changing the App.js with our codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+// Define our app components
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <ul>
+        <li>Define app components</li>
+        <li>Export the components (at the end)</li>
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+// Export the components
+export default App
